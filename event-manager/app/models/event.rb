5a131d7d5db2673c0a86625f7bddb800044d5672
Dvessen83 <d.v.essen@gmail.com>
2017-03-10 11:35:33 +0100

    Fixed foreign key

--- event-manager/app/models/event.rb
+++ event-manager/app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  has_many :visitors
+
   def self.order_by_date
     order(:date)
   end
--- event-manager/db/seeds.rb
+++ event-manager/db/seeds.rb
@@ -5,9 +5,9 @@ event1 = Event.create! ( {name:"Spidergawd", image_url: "http://www.vera-groning
 event2 = Event.create! ( {name:"Dance Valley", image_url: "http://images0.tcdn.nl/binnenland/article21783702.ece/BINARY/u/dance+valley2.jpg", date:"2017-08-12", capacity:5000, genre:"dance"})
 event3 = Event.create! ( {name:"Arctic Space Monkeys", image_url: "https://pbs.twimg.com/profile_images/799566812782399488/KDmPLOCM.jpg", date:"2017-05-05", capacity:1, genre:"Space Rock"})
 
-visitor1 = Visitor.create! ( {first_name:"Caroline", last_name:"Olijve", age:38, image_url:"", payment_method:"cash"})
-visitor2 = Visitor.create! ( {first_name:"JT", last_name:"Breunissen", age:31, payment_method:"cash"})
-visitor3 = Visitor.create! ( {first_name:"Youri", last_name:"Harmsen", age:32, payment_method:"ideal"})
-visitor4 = Visitor.create! ( {first_name:"Dennis", last_name:"Van Essen", age: 33, payment_method:"creditcard"})
+visitor1 = Visitor.create! ( {first_name:"Caroline", last_name:"Olijve", age:38, image_url:"", payment_method:"cash", event_id:1})
+visitor2 = Visitor.create! ( {first_name:"JT", last_name:"Breunissen", age:31, payment_method:"cash", event_id:1})
+visitor3 = Visitor.create! ( {first_name:"Youri", last_name:"Harmsen", age:32, payment_method:"ideal", event_id:1})
+visitor4 = Visitor.create! ( {first_name:"Dennis", last_name:"Van Essen", age: 33, payment_method:"creditcard", event_id:1})
 
 puts "Db created"
