2ead0c6165ed32545e96ac0030801071e55465be
Dvessen83 <d.v.essen@gmail.com>
2017-03-10 14:08:20 +0100

    Made changes to visitor

--- event-manager/app/controllers/visitors_controller.rb
+++ event-manager/app/controllers/visitors_controller.rb
@@ -6,6 +6,44 @@ class VisitorsController < ApplicationController
 
   def show
     @visitor = Visitor.find(params[:id])
-    
+
   end
+
+  def new
+    @visitor = Visitor.new
+
+  end
+
+  def create
+    visitor_params = params.require(:visitor).permit(:first_name, :last_name, :age, :image_url, :payment_method, :event_id)
+
+    @visitor = Visitor.new(visitor_params)
+
+    if @visitor.save
+      redirect_to @visitor
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+    @visitor = Visitor.find(params[:id])
+
+  end
+
+  def update
+    @visitor = Visitor.find(params[:id])
+
+    visitor_params = params.require(:visitor).permit(:first_name, :last_name, :age, :image_url, :payment_method, :event_id)
+
+    if @visitor.update_attributes(visitor_params)
+      redirect_to @visitor
+    else
+      render "edit"
+    end
+
+  end
+
+
 end
